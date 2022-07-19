043fb3625f0d901e1f7767c875f9c16adccc2b6a
Wyrun <maxzubkovie@gmail.com>
2022-07-19 16:09:55 +0200

    Routes and Controllers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
 
 // connect to MongoDB
 var mongoose = require('mongoose');
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +47,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//app.listen(3000, function(){});
+app.listen(3000, function(){});
 
 module.exports = app;
